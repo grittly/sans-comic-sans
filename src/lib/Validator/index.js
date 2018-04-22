705a965aef2f78f32ec167a9b9528d5a63b5bcee
grittly <grittlydev@gmail.com>
2018-04-21 21:31:26 -0400

    Wrote the tests for Validator and the validatorFunctions. Tests are red

--- src/lib/Validator/index.js
+++ src/lib/Validator/index.js
@@ -1,7 +1,41 @@
+import validatorFunctions from './validatorFunctions';
+
 /**
- * Validation function builder that takes in a list of rules and validations and returns a custom function
- * @param {Object} rules - specific options passed into validation functions. eg. allowed characters
- * @param {Array} validations - names of validations to run
+ * Validation function builder that takes in a list of rules
+ * and validations and returns a custom function
+ * @param {Object} rules - a dictionary of specific options passed into validation functions.
+ * eg. allowed characters
+ * @param {Object} validations - dictionary of validations where keys are names of properties
+ * and the values are arrays of validation tests to be tested.
+ * eg. {x: [PRESENCE, ALLOWED_CHARACTERS]}.
+ * Order matters because testing stops at first error encountered
  */
-export default function Validator(rules = {}, validations = []) {
+export default function Validator(rules = {}, validations = {}) {
+  /**
+   * A validation function
+   * @param {string} key
+   * @param {Object} entry - value being validated in the form of {value: number|string, errors: []}
+   * @param {Object} customRules - Add any additional rules
+   * @param {Array} customValidations - Override the list of validations for the given key
+   * @returns {Object} - returns the entry object
+   */
+  return (key, entry, customRules = {}, customValidations = []) => {
+    // Add customRules and customValidations
+    const mergedValidations = customValidations.length > 0 ?
+      customValidations : (validations[key] || []);
+    const mergedRules = { ...rules, ...customRules };
+    const errors = mergedValidations.reduce((acc, validation) => {
+      if (acc.length === 0) {
+        return [
+          ...acc,
+          ...validatorFunctions[validation](entry.value, key, mergedRules),
+        ];
+      }
+      return acc;
+    });
+    return {
+      ...entry,
+      errors,
+    };
+  };
 }
--- test/lib/validator.js
+++ test/lib/validator.js
@@ -1,23 +1,160 @@
 /* globals describe it */
 import { expect } from 'chai';
 import Validator from '../../src/lib/Validator';
-import { 
+import {
   VALIDATE_PRESENCE,
   VALIDATE_ALLOWED_CHARACTERS,
   VALIDATE_CHAR_LENGTH_RANGE,
   VALIDATE_INTEGER,
   VALIDATE_NUMERIC_RANGE,
+  RULE_ALLOWED_CHARACTERS,
+  RULE_NUMERIC_RANGE,
+  RULE_CHAR_LENGTH_RANGE,
 } from '../../src/constants';
 
+const rules = {
+  [RULE_CHAR_LENGTH_RANGE]: [0, 5],
+  [RULE_NUMERIC_RANGE]: [4, 10],
+  [RULE_ALLOWED_CHARACTERS]: /^\S+$/,
+};
+const validations = {
+  presence_test: [VALIDATE_PRESENCE],
+  allowed_chars_test: [VALIDATE_PRESENCE, VALIDATE_ALLOWED_CHARACTERS],
+  char_length_test: [VALIDATE_PRESENCE, VALIDATE_CHAR_LENGTH_RANGE],
+  integer_test: [VALIDATE_PRESENCE, VALIDATE_INTEGER],
+  numeric_range_test: [VALIDATE_PRESENCE, VALIDATE_NUMERIC_RANGE],
+};
+const Validate = Validator(rules, validations);
+
 describe('Validator', () => {
-  it('Handles the case where they key provided is not present in the validations');
-  it('Does not add another error after the first one');
-  it('Adds errors property to the return value even if it is not provided in the entry object');
-  it('Validation: PRESENCE');
-  it('Validation: ALLOWED_CHARACTERS');
-  it('Validation: CHAR_LENGTH_RANGE');
-  it('Validation: INTEGER');
-  it('Validation: NUMERIC_RANGE');
+  it('Handles the case where they key provided is not present in the validations', () => {
+    const key = 'non_existent_key';
+    const entry = { value: 'somevalue', errors: [] };
+    const result = Validate(key, entry);
+    expect(result.errors).to.have.length(0);
+  });
+  it('Does not add another error after the first one', () => {
+    const key = 'allowed_chars_test';
+    const entry = { value: '', errors: [] };
+    const result = Validate(key, entry);
+    expect(result.errors).to.have.length(1);
+    expect(result.errors[0].to.include('empty'));
+  });
+  it('Adds errors property to the return value even if it is not provided in the entry object', () => {
+    const key = 'presence_test';
+    // Invalid value
+    let entry = { value: '' };
+    let result = Validate(key, entry);
+    expect(result).to.have.property('errors');
+    expect(result.errors).to.have.length(1);
 
+    // Valid value
+    entry = { value: 'valid value' };
+    result = Validate(key, entry);
+    expect(result).to.have.property('errors');
+    expect(result.errors).to.have.length(0);
+  });
+  it('Validation: VALIDATE_PRESENCE', () => {
+    const key = 'presence_test';
 
+    // Empty string generates error
+    let entry = { value: '', errors: [] };
+    let result = Validate(key, entry);
+    expect(result.errors).to.have.length(1);
+    expect(result.errors[0]).to.have.string('empty');
+
+    // Non-empty string passes
+    entry = { value: 'notempty', errors: [] };
+    result = Validate(key, entry);
+    expect(result.errors).to.have.length(0);
+
+    // Undefined value fails
+    entry = { value: undefined, errors: [] };
+    result = Validate(key, entry);
+    expect(result.errors).to.have.length(1);
+    expect(result.errors[0]).to.have.string('empty');
+  });
+  it('Validation: VALIDATE_ALLOWED_CHARACTERS', () => {
+    const key = 'allowed_chars_test';
+
+    // Invalid value - a whitespace character
+    let entry = { value: ' space', errors: [] };
+    let result = Validate(key, entry);
+    expect(result.errors).to.have.length(1);
+    expect(result.errors[0]).to.include('invalid characters');
+
+    // Valid value
+    entry = { value: 'nospace', errors: [] };
+    result = Validate(key, entry);
+    expect(result.errors).to.have.length(0);
+  });
+  it('Validation: VALIDATE_CHAR_LENGTH_RANGE', () => {
+    const key = 'char_length_test';
+
+    // Invalid value
+    let entry = { value: 'toomanycharacters', errors: [] };
+    let result = Validate(key, entry);
+    expect(result.errors).to.have.length(1);
+    expect(result.errors[0]).to.include('characters long');
+
+    // Valid value
+    entry = { value: '123', errors: [] };
+    result = Validate(key, entry);
+    expect(result.errors).to.have.length(0);
+  });
+  it('Validation: VALIDATE_INTEGER', () => {
+    const key = 'integer_test';
+    // Invalid values
+    let entry = { value: 'a', errors: [] };
+    let result = Validate(key, entry);
+    expect(result.errors).to.have.length(1);
+    expect(result.errors[0]).to.include('must be an integer');
+
+    entry = { value: '12.3', errors: [] };
+    result = Validate(key, entry);
+    expect(result.errors).to.have.length(1);
+    expect(result.errors[0]).to.include('must be an integer');
+
+    // Valid values
+    entry = { value: '1', errors: [] };
+    result = Validate(key, entry);
+    expect(result.errors).to.have.length(0);
+
+    entry = { value: '0', errors: [] };
+    result = Validate(key, entry);
+    expect(result.errors).to.have.length(0);
+
+    entry = { value: 1, errors: [] };
+    result = Validate(key, entry);
+    expect(result.errors).to.have.length(0);
+  });
+  it('Validation: VALIDATE_NUMERIC_RANGE', () => {
+    const key = 'numeric_range_test';
+
+    // Invalid values
+    let entry = { value: 0, errors: [] };
+    let result = Validate(key, entry);
+    expect(result.errors).to.have.length(1);
+    expect(result.errors[0]).to.include('must be within');
+
+    entry = { value: 100, errors: [] };
+    result = Validate(key, entry);
+    expect(result.errors).to.have.length(1);
+    expect(result.errors[0]).to.include('must be within');
+
+    entry = { value: -3, errors: [] };
+    result = Validate(key, entry);
+    expect(result.errors).to.have.length(1);
+    expect(result.errors[0]).to.include('must be within');
+
+    // Valid values - border case
+    entry = { value: 4, errors: [] };
+    result = Validate(key, entry);
+    expect(result.errors).to.have.length(0);
+
+    // Valid values - within range
+    entry = { value: 7, errors: [] };
+    result = Validate(key, entry);
+    expect(result.errors).to.have.length(0);
+  });
 });
