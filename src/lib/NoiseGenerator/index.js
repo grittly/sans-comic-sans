54556e72a314a15a79088727ffa0ffde6d1347bd
grittly <grittlydev@gmail.com>
2018-04-25 20:47:10 -0400

    Wrote the noiseGenerator library that is the core mechanism behind encrypting the pixels on an image. Also wrote unit tests for its core function. Tests are green

--- src/lib/NoiseGenerator/index.js
+++ src/lib/NoiseGenerator/index.js
@@ -1,10 +1,62 @@
 import seedrandom from 'seedrandom';
 
-  
+/**
+ *  Helper function to generate a sequence of random numbers from 0 to 255
+ *  to be used for every pixel (4 channels per pixel) in a given image
+ *  @param {string} key - a string that is used as seed for the pseudo random number generator
+ *  @param {number} width - width of image in pixels
+ *  @param {number} height - height of image in pixels
+ */
+function generateNoise(key, width, height) {
+  const rng = seedrandom(key);
+  const randomSequence = [];
+  for (let i = 0; i < width * height * 4; i += 4) {
+    // Red channel
+    randomSequence[i] = Math.round(rng() * 255);
+    // Green channel
+    randomSequence[i + 1] = Math.round(rng() * 255);
+    // Blue channel
+    randomSequence[i + 2] = Math.round(rng() * 255);
+    // Alpha channel
+    randomSequence[i + 3] = 255;
+  }
+  return randomSequence;
+}
 
 /**
- *  Generate
+ *  Add pixels from noiseMap to pixel data from imageData or encryptedData in modulo 255
+ *  @param {Uint8ClampedArray} imageData - image data to add noise too
+ *  @param {Uint8ClampedArray} encryptedData - image data layer that already
+ *  contains previously encrypted areas. Default is an empty array. This is
+ *  necessary for encrypting overlapping areas. Noise is added using values of
+ *  this layer if there non transparent values at a given pixel.
+ *  @param {Uint8CalampedArray} noiseData - noise data to be added to pixels
+ *  @param {boolean} decrypt - if true, noise map is subtracted, otherwise it is added
+ *  @return {Uint8ClampedArray} - image data that is to be added to the encryptedData layer
  */
-export default function addNoise() {
+export function mergeNoiseAndImageData(imageData, encryptedData, noiseData, decrypt = false) {
+  const newData = new Uint8ClampedArray(imageData);
+  const modulo = 256;
+  const direction = decrypt ? -1 : 1;
+  for (let i = 0; i < newData.length; i += 4) {
+    // Red channel
+    newData[i] = (
+      (encryptedData[i + 3] ? encryptedData[i] : newData[i])
+      + (noiseData[i] * direction)) % modulo;
+    // Green channel
+    newData[i + 1] = (
+      (encryptedData[i + 3] ? encryptedData[i + 1] : newData[i + 1])
+      + (noiseData[i + 1] * direction)) % modulo;
+    // Blue channel
+    newData[i + 2] = ((encryptedData[i + 3] ? encryptedData[i + 2] : newData[i + 2])
+      + (noiseData[i + 2] * direction)) % modulo;
+    // Alpha channel
+    newData[i + 3] = 255;
+  }
+  return newData;
+}
 
+export default function encryptArea(key, imageData, encryptedData, decrypt) {
+  const noiseMap = generateNoise(key, imageData.width, imageData.height);
+  return mergeNoiseAndImageData(imageData, encryptedData, noiseMap, decrypt);
 }
---
+++ test/lib/noiseGenerator.js
@@ -0,0 +1,60 @@
+/* globals describe it */
+import { expect } from 'chai';
+import { mergeNoiseAndImageData } from '../../src/lib/NoiseGenerator';
+
+
+// Generate a typed array equivalent to a 25 by 25 pixels image,
+// where every pixel equals RGBA(100,100, 255)
+const pixelsArray = Array.from({ length: 25 * 25 }, () => [100, 100, 100, 255])
+  .reduce((acc, pixel) => [...acc, ...pixel]);
+const emptyMap = Uint8ClampedArray
+  .from(pixelsArray, () => 0);
+
+
+const dataImageRegular = Uint8ClampedArray
+  .from(Array.from(
+    { length: 25 * 25 },
+    () => [100, 100, 100, 255],
+  )
+    .reduce((acc, pixel) => [...acc, ...pixel]));
+
+const dataImagePlus = Uint8ClampedArray
+  .from(Array.from(
+    { length: 25 * 25 },
+    () => [100 + 10, 100 + 10, 100 + 10, 255],
+  )
+    .reduce((acc, pixel) => [...acc, ...pixel]));
+
+const dataImageMinus = Uint8ClampedArray
+  .from(Array.from(
+    { length: 25 * 25 },
+    () => [100 - 10, 100 - 10, 100 - 10, 255],
+  )
+    .reduce((acc, pixel) => [...acc, ...pixel]));
+
+const noiseMap = Uint8ClampedArray
+  .from(Array.from(
+    { length: 25 * 25 },
+    () => [10, 10, 10, 255],
+  )
+    .reduce((acc, pixel) => [...acc, ...pixel]));
+
+describe('Validator', () => {
+  it('Validate that adding noise works', () => {
+    const imagePlusNoise = mergeNoiseAndImageData(dataImageRegular, emptyMap, noiseMap, false);
+    expect(imagePlusNoise.slice(0, 100)).to.eql(dataImagePlus.slice(0, 100));
+  });
+  it('Validate that subtracting noise works', () => {
+    const imageMinusNoise = mergeNoiseAndImageData(dataImageRegular, emptyMap, noiseMap, true);
+    expect(imageMinusNoise.slice(0, 100)).to.eql(dataImageMinus.slice(0, 100));
+  });
+  it('Validate that noise map adds to pixels in encryptedLayer if they are present', () => {
+    const imagePlusNoise = mergeNoiseAndImageData(
+      dataImageRegular,
+      dataImageMinus,
+      noiseMap,
+      false,
+    );
+    expect(imagePlusNoise.slice(0, 100)).to.eql(dataImageRegular.slice(0, 100));
+  });
+});
